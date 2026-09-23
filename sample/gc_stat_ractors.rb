3934c4dbfeb7ccc9466ca66e32d25f381a02ce9b
Matt Valentine-House <matt@eightbitraptor.com>
2026-09-23 14:36:54 +0100

    Add a per-Ractor GC timing example

---
+++ sample/gc_stat_ractors.rb
@@ -0,0 +1,25 @@
+Warning[:experimental] = false
+GC.measure_total_time = true
+
+workers = 4.times.map do |worker_id|
+  Ractor.new(worker_id) do |id|
+    Ractor.receive # Wait until all workers have been created.
+    20.times do |batch|
+      Array.new(10_000) { |i| "#{id}:#{batch}:#{i}".reverse }.sort!
+    end
+    GC.start(full_mark: false)
+    {
+      ractor: "worker-#{id}",
+      marking_time_ms: GC.stat(:marking_time, scope: :ractor),
+      sweeping_time_ms: GC.stat(:sweeping_time, scope: :ractor)
+    }
+  end
+end
+
+workers.each { |worker| worker.send(:start) }
+workers.each { |worker| p worker.value }
+p({
+  ractor: "main",
+  marking_time_ms: GC.stat(:marking_time, scope: :ractor),
+  sweeping_time_ms: GC.stat(:sweeping_time, scope: :ractor)
+})
